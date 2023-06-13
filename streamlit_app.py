144484f29fadd3e914d79d1031ee7cf158f20914
Andrian <andrianprakasa.06@gmail.com>
2023-06-13 19:13:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,7 +88,7 @@ st.markdown("# Tree")
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import graphviz
+import graphviz as graphviz
 from sklearn.preprocessing import LabelEncoder
 from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
