a1b428ec460adaf28318e1b16c898d642992a855
Andrian <andrianprakasa.06@gmail.com>
2023-06-13 19:07:22 +0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,6 +88,7 @@ st.markdown("# Tree")
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import graphviz
 from sklearn.preprocessing import LabelEncoder
 from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
