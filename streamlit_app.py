99dd28a214f212c128c8442955919d90e63a757c
Andrian <andrianprakasa.06@gmail.com>
2023-06-13 18:49:47 +0700

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,3 +37,49 @@ loss, accuracy = ann.evaluate(x_test, y_test)
 
 st.write(f"Test Loss: {loss:.4f}")
 st.write(f"Test Accuracy: {accuracy:.4f}")
+
+st.markdown("# Naive Bayess")
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import streamlit as st
+
+# Load the dataset
+dataset_path = "https://raw.githubusercontent.com/AndrianPrakasa/UAS/main/CSV/csa.csv"
+data = pd.read_csv(dataset_path)
+data.replace("Agree", 1, inplace=True)
+data.replace("Yes", 1, inplace=True)
+data.replace("No", 0, inplace=True)
+data.replace("Disagree", 0, inplace=True)
+
+# Display the dataset in Streamlit
+st.dataframe(data.head())
+
+X = data.iloc[:, :5].values
+y = data['Knowledge Level'].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+accuracy = accuracy_score(y_test, y_pred)
+
+# Display accuracy score
+st.write(f"Accuracy: {accuracy}")
+
+df = pd.DataFrame({'Real Values': y_test, 'Predicted Values': y_pred})
+st.dataframe(df)
+
