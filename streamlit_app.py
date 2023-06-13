dfa9680c997472ddaffbd88b20a2f33e97120a6f
Andrian <andrianprakasa.06@gmail.com>
2023-06-13 19:24:53 +0700

    q

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,11 +88,11 @@ st.markdown("# Tree")
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import pygraphviz as graphviz
+import pygraphviz as pgv
 from sklearn.preprocessing import LabelEncoder
 from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import streamlit as st
 
@@ -117,7 +117,7 @@ y = df_copy['Knowledge Level'].values
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Create a decision tree classifier with a maximum depth of 3 and Gini impurity criterion.
+# Create a decision tree classifier with a maximum depth of 3 and Gini impurity criterion
 clf_gini = DecisionTreeClassifier(criterion="gini", max_depth=3, random_state=0)
 
 # Fit the model
@@ -125,10 +125,10 @@ clf_gini.fit(X_train, y_train)
 
 # Export the decision tree to a Graphviz object
 dot_data = tree.export_graphviz(clf_gini, out_file=None, filled=True, rounded=True, special_characters=True)
-graph = graphviz.Source(dot_data)
+graph = pgv.AGraph(string=dot_data)
 
 # Display the decision tree in Streamlit
-st.graphviz_chart(graph)
+st.graphviz_chart(graph.draw(format='png'))
 
 # Calculate accuracy on the training set
 y_pred_train_gini = clf_gini.predict(X_train)
@@ -137,5 +137,6 @@ st.write("Accuracy on training set (Gini): {:.3f}".format(accuracy_score(y_train
 # Calculate accuracy on the testing set
 y_pred_gini = clf_gini.predict(X_test)
 st.write("Accuracy on testing set (Gini): {:.3f}".format(accuracy_score(y_test, y_pred_gini)))
+
 
 
