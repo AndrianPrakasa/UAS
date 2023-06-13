8dbb15530dcd6753d91765e7db2e4641d8545d5e
Andrian <andrianprakasa.06@gmail.com>
2023-06-13 18:59:03 +0700

    Final

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,3 +83,59 @@ st.write(f"Accuracy: {accuracy}")
 df = pd.DataFrame({'Real Values': y_test, 'Predicted Values': y_pred})
 st.dataframe(df)
 
+st.markdown("# Tree")
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import graphviz
+from sklearn.preprocessing import LabelEncoder
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import accuracy_score
+import streamlit as st
+
+# Load the dataset
+dataset = "https://raw.githubusercontent.com/AndrianPrakasa/UAS/main/CSV/csa.csv"
+df = pd.read_csv(dataset)
+df.replace("Agree", 1, inplace=True)
+df.replace("Yes", 1, inplace=True)
+df.replace("No", 0, inplace=True)
+df.replace("Disagree", 0, inplace=True)
+
+# Display the dataset in Streamlit
+st.dataframe(df.head())
+
+df_copy = df.copy()
+df_copy.isnull().sum()
+
+df_copy.info()
+
+X = df_copy.iloc[:, :5].values
+y = df_copy['Knowledge Level'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a decision tree classifier with a maximum depth of 3 and Gini impurity criterion.
+clf_gini = DecisionTreeClassifier(criterion="gini", max_depth=3, random_state=0)
+
+# Fit the model
+clf_gini.fit(X_train, y_train)
+
+# Export the decision tree to a Graphviz object
+dot_data = tree.export_graphviz(clf_gini, out_file=None, filled=True, rounded=True, special_characters=True)
+graph = graphviz.Source(dot_data)
+
+# Display the decision tree in Streamlit
+st.graphviz_chart(graph)
+
+# Calculate accuracy on the training set
+y_pred_train_gini = clf_gini.predict(X_train)
+st.write("Accuracy on training set (Gini): {:.3f}".format(accuracy_score(y_train, y_pred_train_gini)))
+
+# Calculate accuracy on the testing set
+y_pred_gini = clf_gini.predict(X_test)
+st.write("Accuracy on testing set (Gini): {:.3f}".format(accuracy_score(y_test, y_pred_gini)))
+
+
